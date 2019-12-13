a0132a5ddcf9959e18289f761d0074773c63c7ae
kamalhaddad04 <kdawg4.956@gmail.com>
2019-12-12 19:56:17 -0500

    Updates

---
+++ precache-manifest.727a132a747a7926f2c098b4d663e956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae580850a54d8fa927831c4745b0dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72b92214a9f52b2c74c3",
+    "url": "/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "b96e3536bc4fd2a01a6c",
+    "url": "/static/css/main.30622e2e.chunk.css"
+  },
+  {
+    "revision": "72b92214a9f52b2c74c3",
+    "url": "/static/js/2.5c60e3e6.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.5c60e3e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b96e3536bc4fd2a01a6c",
+    "url": "/static/js/main.7ac9e8fa.chunk.js"
+  },
+  {
+    "revision": "c18f8ae3a8a6413c8e3d",
+    "url": "/static/js/runtime-main.6f8d6909.js"
+  }
+]);
